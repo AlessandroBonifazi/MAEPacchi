a8ed9870b6fa53d989f103a8ef1331227ed8899d
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2023-02-18 23:33:14 +0100

    Changed Route

--- App_Start/RouteConfig.cs
+++ App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MAEPacchi
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Pacchi", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
