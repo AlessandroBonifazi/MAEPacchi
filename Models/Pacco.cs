abf5140f23f6e32f91fe8e33dc67424232a42c69
AlessandroBonifazi <alessandrobonifazi21@gmail.com>
2023-02-18 23:55:02 +0100

    Added model

---
+++ Models/Pacco.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+//using System.Data.Entity;
+
+namespace MAEPacchi.Models
+{
+    public class Pacco
+    {
+        public int ID { get; set; }
+        public string Sender { get; set; }
+        public string Recipient { get; set; }
+        public DateTime Date { get; set; }
+        public string Note { get; set; }
+        public decimal Price { get; set; }
+    }
+    //public class MovieDBContext : DbContext
+    //{
+    //    public DbSet<Movie> Movies { get; set; }
+    //}
+}
